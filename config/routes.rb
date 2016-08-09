785327da3f7b4699281435d9004d1f67bf686ebe
pmNiko <nikolas090189@gmail.com>
2016-08-09 00:07:40 -0300

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   root "articles#index"
+
 
   get '/articles',
       to: 'articles#index',
@@ -9,10 +11,17 @@ Rails.application.routes.draw do
   get '/articles/new',
       to: 'articles#new',
       as: 'new_article'
+  get '/articles/:id/edit',
+      to: 'articles#edit',
+      as: 'edit_article'
   get 'articles/:id',
       to: 'articles#show',
       as: 'article'
+  patch '/articles/:id',
+      to: 'articles#update'
   delete '/articles/:id',
       to: 'articles#destroy'
-  #resource :articles
+=begin
+  resource :articles
+=end
 end
